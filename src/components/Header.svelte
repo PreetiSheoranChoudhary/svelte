199e853c9c776b2438b36e9d8e10807d3dd7e91f
Preeti <preeti09cse@gmail.com>
2024-10-03 15:07:50 +0530

    other half

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,22 +1,41 @@
 <script>
-
+    import { openModal } from "../store";
 </script>
-<header class="flex flex-col relative z-20">
-    <div class=" max-w-[1400px] mx-auto w-full  p-4 flex  items-center justify-between py-6  ">
-    <a href="/">
-        <h1 class="font-semibold">swoley <span class="text-indigo-400"> moley</span></h1>
-    </a>
-    <button class=" md:hidden grid place-items-center">
-        <i class="fa-solid fa-bars"></i>
-    </button>
-    <nav class=" hidden md:flex item-center gap-4 lg:gap-6">
-<a href="#product" class=" duration-200 hover:text-indigo-400 cursor-pointer"> Product</a>
-<a href="#reviews" class=" duration-200 hover:text-indigo-400 cursor-pointer"> Reviews</a>
-<a href="#FAQs" class=" duration-200 hover:text-indigo-400 cursor-pointer">FAQs </a>
-<button class="specialBtn">
-    <p> start free today</p>
-</button>
-    </nav>
-    </div>
 
+<header class="flex flex-col relative z-20">
+    <div
+        class="max-w-[1400px] mx-auto w-full flex items-center justify-between p-4 py-6"
+    >
+        <a href="/">
+            <h1 class="font-semibold">
+                Swoley <span class="text-indigo-400">Moley</span>
+            </h1>
+        </a>
+        <button
+            on:click={() => ($openModal = true)}
+            class="md:hidden grid place-items-center"
+        >
+            <i class="fa-solid fa-bars"></i>
+        </button>
+        <nav class="hidden md:flex items-center gap-4 lg:gap-6">
+            <a
+                href="#product"
+                class="duration-200 hover:text-indigo-400 cursor-pointer"
+                >Product</a
+            >
+            <a
+                href="#reviews"
+                class="duration-200 hover:text-indigo-400 cursor-pointer"
+                >Reviews</a
+            >
+            <a
+                href="#faqs"
+                class="duration-200 hover:text-indigo-400 cursor-pointer"
+                >FAQs</a
+            >
+            <button class="specialBtn">
+                <p>Start free today</p>
+            </button>
+        </nav>
+    </div>
 </header>
