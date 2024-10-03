0cde7ea9982cd7637e572c8a2ec405a51bddf71d
Preeti <preeti09cse@gmail.com>
2024-10-03 15:07:38 +0530

    schema layout added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,15 @@
 <script>
+    import Conversion from "../components/Conversion.svelte";
+    import FaQs from "../components/FAQs.svelte";
     import Hero from "../components/Hero.svelte";
-
-
+    import Product from "../components/Product.svelte";
+    import Reviews from "../components/Reviews.svelte";
 </script>
-<main>
-    <Hero/>
+
+<main class="flex flex-col">
+    <Hero />
+    <Product />
+    <Reviews />
+    <FaQs />
+    <Conversion />
 </main>
