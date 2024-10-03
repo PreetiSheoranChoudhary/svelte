f87de99437ca45a11c383456596cbe37c8fb6e71
Preeti <preeti09cse@gmail.com>
2024-10-03 15:07:07 +0530

    review is added

--- src/components/SectionWrapper.svelte
+++ src/components/SectionWrapper.svelte
@@ -1,6 +1,9 @@
-<scerion class={"min-h-screen flex flex-col px-4"}>
-<div class="flex flex-col flex-1 max-w-[1400px] mx-auto w-full ">
-    <slot></slot>
+<script>
+    export let id;
+</script>
 
-</div>
-</scerion>
+<section {id} class={"min-h-screen flex flex-col px-4 "}>
+    <div class="flex flex-col flex-1 max-w-[1400px] mx-auto w-full">
+        <slot />
+    </div>
+</section>
